497f3936cf2e092e3e60e27281761d56607e9492
i-Living <living.59@yandex.ru>
2026-06-17 02:53:53 +0300

    Добавлено описание приложения и секция 'Как TimeBox использует данные Google' для OAuth-верификации

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,7 @@ import { render } from 'preact';
 import { App } from './app';
 import './index.css';
 
+// Hide the static info section when PWA mounts (Google verifier can read it without JS)
+document.documentElement.classList.add('js-loaded');
+
 render(<App />, document.getElementById('app')!);
