0ec2f68cabface3fdc87f40c33e461a07b3c1608
i-Living <i-Living@users.noreply.github.com>
2026-06-18 05:19:45 +0000

    deploy: f54a89cc87f6e71de0e3972083b29b3349591466

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/timebox/sw.js', { scope: '/timebox/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
